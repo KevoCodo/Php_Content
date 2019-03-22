0c6d3059754e2c73379a9295fc55959a6ce77535
Kevin Gay <kevin.gay1@gmail.com>
2019-03-21 23:03:17 -0400

    Updated website4 and added website5

---
+++ website4/page1.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST['submit'])){
+    session_start();
+    
+    $_SESSION['name'] = htmlentities($_POST['name']);
+    $_SESSION['email'] = htmlentities($_POST['email']);
+  
+    header('Location: page2.php');
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Sessions</title>
+</head>
+<body>
+  <form method='post' action="<?php $_SERVER['PHP_SELF'] ?>">
+    <div class="form-group">
+      <label >Name</label>
+      <input type="text" class="form-control" name="name" placeholder="Enter Name">
+    </div>
+    <div class="form-group">
+      <label >Email</label>
+      <input type="email" class="form-control" name='email' placeholder="Enter Email Address">
+    </div><br/><br/>
+    <button type="submit" class="btn btn-primary" name='submit'>Submit</button>
+  </form>
+</body>
+</html>
---
+++ website4/page4.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    print_r($_SESSION);
+    
+    unset($_SESSION['name']);
+    //session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Sessions</title>
+</head>
+<body>
+    
+</body>
+</html>
